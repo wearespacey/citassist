e6b18ed7e5d0258cb6685b6be2b05aee3d43e93e
Arsero <azzedine.develop@gmail.com>
2020-03-07 19:12:49 +0100

    Add dashboard

--- web/citassist/src/components/dashboard/index.js
+++ web/citassist/src/components/dashboard/index.js
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import { Grid, GridRow, GridColumn, Form, Header, Segment, Image, Input, Button, Container } from 'semantic-ui-react';
 import './dashboard.css';
+import Logo from '../../logo.png';
 
 export default ()=>{
     return(
-        <p>Dashboard component</p>
-    )
+        <div>
+            <header>
+                <Image src={Logo} size='large' centered/>
+            </header>
+            <Grid className='dashboard_container'>
+                <Grid.Column className='dashboard_menu_left' width={5} fluid>
+                    <p>Hello</p>
+                </Grid.Column>
+                <Grid.Column width={6}>
+                    
+                </Grid.Column>
+                <Grid.Column className='dashboard_menu_right' width={5} fluid>
+                    <p>Hello</p>
+                </Grid.Column>
+            </Grid>
+        </div>
+    );
 }
--- web/citassist/src/components/login/index.js
+++ web/citassist/src/components/login/index.js
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import { Grid, GridRow, GridColumn, Form, Header, Segment, Image, Message, Input, Button, Loader } from 'semantic-ui-react';
 import './login.css';
-import Logo from './logo.png';
+import Logo from '../../logo.png';
 import { Redirect, Route } from 'react-router-dom';
 
 export default (props)=>{
@@ -17,25 +17,25 @@ export default (props)=>{
 
     return(
         <Grid className='login_container' centered inverted padded>
-                <GridColumn computer={6} tablet={10} mobile={16} >
-                    <GridRow centered>
-                        <Image src={Logo} size='medium' centered/>
-                    </GridRow>
-                    <GridRow centered>
-                        <Form as={Segment} textAlign='center' size={'big'}>
-                            <Form.Input name='username' onChange={event=>setUsername(event.target.value)} icon='user' iconPosition='left' placeholder='Username' fluid />
-                            <Form.Input name='password' onChange={event=>setPassword(event.target.value)} icon='lock' iconPosition='left' placeholder='Password' type='password' fluid/>
-                            <Form.Button onClick={()=>connect()} fluid color='green'>Log In</Form.Button>
-                            {
-                                isLogged ?
-                                <Redirect to = {{ pathname: '/dashboard', state: { from: props.location } }} />
-                                :
-                                null
-                            }
-                            {/* <Loader active /> */}
-                        </Form>
-                    </GridRow>
-                </GridColumn>
-            </Grid>
+            <GridColumn computer={6} tablet={10} mobile={16} >
+                <GridRow centered>
+                    <Image src={Logo} size='medium' centered/>
+                </GridRow>
+                <GridRow centered>
+                    <Form as={Segment} textAlign='center' size={'big'}>
+                        <Form.Input name='username' onChange={event=>setUsername(event.target.value)} icon='user' iconPosition='left' placeholder='Username' fluid />
+                        <Form.Input name='password' onChange={event=>setPassword(event.target.value)} icon='lock' iconPosition='left' placeholder='Password' type='password' fluid/>
+                        <Form.Button onClick={()=>connect()} fluid color='green'>Log In</Form.Button>
+                        {
+                            isLogged ?
+                            <Redirect to = {{ pathname: '/dashboard', state: { from: props.location } }} />
+                            :
+                            null
+                        }
+                        {/* <Loader active /> */}
+                    </Form>
+                </GridRow>
+            </GridColumn>
+        </Grid>
     );
 }
