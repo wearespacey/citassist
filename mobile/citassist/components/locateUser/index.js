520611c1f8a72851deb0b9f46ec330396d6cce3e
xvercruysse <vercruysse.xavier@xvercruysse.tech>
2020-03-07 16:20:47 +0100

    test locateUser

--- mobile/citassist/components/locateUser/index.js
+++ mobile/citassist/components/locateUser/index.js
@@ -1,14 +1,19 @@
-import { NetworkInfo } from 'react-native-network-info';
+import WifiManager from 'react-native-wifi-reborn';
 import React, { useState } from 'react';
 import { View, Text } from 'react-native';
 
 export default function LocateUser() {
     const [ap, setAp] = useState();
     // Get BSSID
-    NetworkInfo.getSSID().then(bssid => {
-        console.log(bssid);
-        setAp(bssid);
-    });
+    WifiManager.getCurrentWifiSSID().then(
+        ssid => {
+            console.log('Your current connected wifi SSID is ' + ssid);
+            setAp(ssid);
+        },
+        () => {
+            console.log('Cannot get current SSID!');
+        }
+    );
 
     const accessPoints = ['Accueil', 'Parking', 'Urgences']; //Ceci est une API
     const isAtHospital = accessPoints.includes(ap);
