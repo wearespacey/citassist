6defb200a2be90511206f1c972acaad24796e7b3
Guillaume <gservais98@gmail.com>
2020-03-08 01:56:33 +0100

    adding ws to mobile app

--- mobile/citassist/service/api.js
+++ mobile/citassist/service/api.js
@@ -1,3 +1,5 @@
+import {Alert} from 'react-native';
+
 const API_URL = 'https://winassist.azurewebsites.net';
 
 const login = async (body)=>{
@@ -9,7 +11,24 @@ const login = async (body)=>{
         body:JSON.stringify(body)
     });
     const jsonReponse = await response.json();
+    if(jsonReponse.data)
+        connectToSocket();
     return jsonReponse.data? jsonReponse.data.token : null;
+}
+
+const connectToSocket = ()=>{
+    const signalR = require("@microsoft/signalr");
+ 
+    let connection = new signalR.HubConnectionBuilder()
+        .withUrl(`${API_URL}/arrivalhub`)
+        .build();
+    
+    connection.on("ReceiveMessage", data => {
+        Alert.alert('Message', data.toString())
+        console.log(data);
+    });
+    
+    connection.start();
 }
 
 export {
